a478e091e16f2bb5ed30090092254e434e5f0672
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-12-18 17:54:11 +0600

    added user crud operation

--- apps/auth-microservice/src/users/users-microservice.controller.ts
+++ apps/auth-microservice/src/users/users-microservice.controller.ts
@@ -1,4 +1,61 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UsersMicroserviceService } from './users-microservice.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller()
-export class UsersMicroserviceController {}
+@Controller('users')
+export class UsersMicroserviceController {
+  constructor(
+    private readonly usersMicroserviceService: UsersMicroserviceService,
+  ) {}
+
+  @Post()
+  @UsePipes(new ValidationPipe())
+  public async createUser(@Body() dto: CreateUserDto) {
+    return await this.usersMicroserviceService.createUser(dto);
+  }
+
+  @Get(':email')
+  public async findUserByEmail(@Param('email') email: string) {
+    return await this.usersMicroserviceService.findUserByEmail(email);
+  }
+
+  @Get(':id')
+  public async findUserById(@Param('id') id: string) {
+    const user = await this.usersMicroserviceService.findUserById(parseInt(id));
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
+  }
+
+  @Delete(':id')
+  public async deleteUserById(@Param('id') id: string) {
+    return await this.usersMicroserviceService.deleteUserById(parseInt(id));
+  }
+
+  @Patch(':id')
+  @UsePipes(new ValidationPipe())
+  public async updateUserById(
+    @Param('id') id: string,
+    @Body() dto: UpdateUserDto,
+  ) {
+    return await this.usersMicroserviceService.updateUserById(
+      parseInt(id),
+      dto,
+    );
+  }
+}
--- apps/auth-microservice/src/users/users-microservice.service.ts
+++ apps/auth-microservice/src/users/users-microservice.service.ts
@@ -1,4 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { genSaltSync, hashSync } from 'bcrypt';
 
 @Injectable()
-export class UsersMicroserviceService {}
+export class UsersMicroserviceService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  public async createUser(user: Partial<UserEntity>) {
+    const hashedPassword = this.hashPassword(user.password);
+    const newUser = this.userRepository.create({
+      email: user.email,
+      password: hashedPassword,
+    });
+
+    return await this.userRepository.save(newUser);
+  }
+
+  public async findUserByEmail(email: string) {
+    return await this.userRepository.findOne({ where: { email } });
+  }
+
+  public async findUserById(id: number) {
+    if (!id) {
+      return null;
+    }
+
+    return await this.userRepository.findOne({ where: { id } });
+  }
+
+  public async deleteUserById(id: number) {
+    const user = await this.findUserById(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return await this.userRepository.remove(user);
+  }
+
+  public async updateUserById(id: number, attrs: Partial<UserEntity>) {
+    const user = await this.findUserById(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    Object.assign(user, attrs);
+    return await this.userRepository.save(user);
+  }
+
+  private hashPassword(password: string) {
+    return hashSync(password, genSaltSync(10));
+  }
+}
